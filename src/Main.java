192f40f63bf4b8c000ce13c5728977d7b5d1dba4
Vladimir <lil.rhymes9@gmail.com>
2023-11-11 00:53:24 +0300

    Peaceful mode added

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,34 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        final boolean I_DONT_LIKE_CONFLICTS = false; // Если вы не хотите споров, просто измените значение на true
+        
+        
+        
         EggThreadTask eggThreadTask = new EggThreadTask();
         ChickenThreadTask chickenThreadTask = new ChickenThreadTask();
 
         Thread chickenThread = new Thread(chickenThreadTask);
         Thread eggThread = new Thread(eggThreadTask);
 
+        chickenThread.setDaemon(true);
         chickenThread.start();
+
+
+        eggThread.setDaemon(true);
         eggThread.start();
 
-        String winner = "";
-        while (true){
-            if (!chickenThread.isAlive()){
-                winner = "Яйцо";
-                break;
-            }
-            if (!eggThread.isAlive()){
-                winner = "Курица";
-                break;
+
+        String winner = "Ничья, дружба";
+        if (!I_DONT_LIKE_CONFLICTS){
+            while (true){
+                if (!chickenThread.isAlive()){
+                    winner = "Яйцо";
+                    break;
+                }
+                if (!eggThread.isAlive()){
+                    winner = "Курица";
+                    break;
+                }
             }
         }
         System.out.println("Победитель - " + winner);
