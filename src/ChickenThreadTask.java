63696e64204bc7303e27776e9036621e13e4d8f9
Vladimir <lil.rhymes9@gmail.com>
2023-11-11 00:35:04 +0300

    Initial commit

---
+++ src/ChickenThreadTask.java
@@ -0,0 +1,6 @@
+public class ChickenThreadTask implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("Первой была курица!");
+    }
+}
---
+++ src/EggThreadTask.java
@@ -0,0 +1,6 @@
+public class EggThreadTask implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("Первым было яйцо!");
+    }
+}
